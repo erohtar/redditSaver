1036e903047d201d9d9a5f95b92eea00facb8f1f
erohtar <mr.rajat@gmail.com>
2022-11-08 19:58:52 +0530

    Added base files

---
+++ README.md
@@ -0,0 +1,38 @@
+# redditSaver
+**Automatically Download Saved Reddit Posts & Comments as Markdown Notes (into Obsidian)**
+
+Sample output:
+![image](https://user-images.githubusercontent.com/5120628/200579098-6311b7f4-0a72-45df-9490-705c25bee720.png)
+
+
+## Features
+- YAML Frontmatter is downloaded along with the markdown formatted note - supports using it with **Dataview** plugin
+- Notes are saved in `reddit\<subreddit>\<note_title>` hierarchy
+- Clickable link to the post/comment is added at the top of the note (besides being saved in Frontmatter)
+
+
+## Setup
+1. You go to [this url](https://ssl.reddit.com/prefs/feeds/) and copy this link:
+![image](https://user-images.githubusercontent.com/5120628/200579587-48bad4e3-e569-4417-a76c-3e88f2353fa7.png)
+
+2. Put that link, and your notes target path in project's **settings.json** file:
+```
+{
+	"jsonUrl": "https://www.reddit.com/user/erohtar/saved.json?feed=xxxxxxxxx&user=erohtar",
+	"rootPath": "C:\\ObsidianVault\\Reddit"
+}
+```
+
+3. Download ([optionally portable](https://github.com/garethflowers/nodejs-portable/)) **node.js** and run **redditSaver.js** with it:
+`<path>\node\node.exe redditSaver.js`
+
+4. Done. Your notes should be created at the set path:
+`<vault>\Reddit\<subreddit>\<title>__<id>.md`
+
+
+## Additional Notes
+- Reddit limits last 25 saved posts/comments in that feed
+- Scheduled running of this script once daily (or so) will keep your latest saves updated automatically
+- This is NOT an Obsidian plugin, and if someone wants to create one based on this idea or my code, they're more than welcome to
+- To save reddit *crossposts*, go to the target posts and save that instead of the link post (a habit I'll have to get into myself as well)
+
---
+++ redditSaver.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const fs = require('fs');  //filesystem
+const https = require('https');
+const wDir = path.dirname(__filename);  //set working dir to script dir
+
+//read app settings
+const settings = JSON.parse(fs.readFileSync(wDir + '\\settings.json'));
+
+//delete old json if exists
+fs.unlinkSync(wDir + '\\saved.json');
+
+//download latest json AND create notes once download finishes
+https.get(settings.jsonUrl,(res) => {
+	const filePath = fs.createWriteStream(wDir + '\\saved.json');
+	res.pipe(filePath);
+	filePath.on('finish',() => {
+		filePath.close();
+		console.log('saved.json : Downloaded');
+		createNotes();
+	})
+})
+
+
+//read json and create notes
+function createNotes() {
+	//create root folder if doesn't already exist
+	newDir(settings.rootPath);
+	
+	//read downloaded json
+	const obj = JSON.parse(fs.readFileSync(wDir + '\\saved.json'));
+		
+	for(let child in obj.data.children) {
+		let thisSub = obj.data.children[child].data.subreddit;
+		let thisAuthor = obj.data.children[child].data.author;
+		let thisUrl = 'https://www.reddit.com' + obj.data.children[child].data.permalink;
+		let thisId = obj.data.children[child].data.id;
+		let thisTimestamp = new Date(obj.data.children[child].data.created_utc * 1000);
+		
+		let thisType;
+		let thisTitle;
+		let thisBody;
+		
+		if(obj.data.children[child].data.title)
+		{
+			thisType = 'post';
+			thisTitle = obj.data.children[child].data.title;
+			thisBody = obj.data.children[child].data.selftext;
+		}
+		else
+		{
+			thisType = 'comment';
+			thisTitle = obj.data.children[child].data.link_title;
+			thisBody = obj.data.children[child].data.body;
+		}
+		
+		//some cleanup
+		thisBody = thisBody.replace(/&amp;#x200B;/g,'')  //some weird chars
+		thisBody = thisBody.replace(/&amp;nbsp;/g,'')  //some weird chars
+		thisTitle = thisTitle.replace(':','-')  //breaks YAML frontmatter
+		
+		let thisNote = '---'
+			+ '\nsub: ' + thisSub
+			+ '\ntitle: ' + thisTitle
+			+ '\ntype: ' + thisType
+			+ '\nauthor: ' + thisAuthor
+			+ '\nurl: ' + thisUrl
+			+ '\ntime: ' + thisTimestamp
+			+ '\nid: ' + thisId
+			+ '\n---\n[link](' + thisUrl + ')'
+			+ '\n' + thisBody;
+		
+		let thisNoteFile = obj.data.children[child].data.permalink.toString().match(/\/comments\/[a-z0-9]+\/([a-z0-9_]+)\//)[1] + '__' + thisId+ '.md';
+		
+		//create folder for sub if it doesn't exist
+		newDir(settings.rootPath + '\\' + thisSub);
+		
+		//create note file
+		console.log('Writing : ' + settings.rootPath + '\\' + thisSub + '\\' + thisNoteFile);
+		if (fs.existsSync(settings.rootPath + '\\' + thisSub + '\\' + thisNoteFile)){
+			fs.unlinkSync(settings.rootPath + '\\' + thisSub + '\\' + thisNoteFile);
+		}
+		fs.appendFileSync(settings.rootPath + '\\' + thisSub + '\\' + thisNoteFile, thisNote);
+	}
+}
+
+function newDir(dir) {
+	if (fs.existsSync(dir)) {
+		//console.log('Dir exists. Do nothing');
+	} else {
+		//console.log('Dir does not exist. So make one');
+		fs.mkdirSync(dir, true)	//recursive: true
+	}
+}
